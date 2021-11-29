876944af18184cfef072072f5ff44467be936ccd
Hélio Neto <helioneto29@hotmail.com>
2021-11-29 11:39:35 -0300

    Iniciando interface do jogo

---
+++ display.py
@@ -0,0 +1,15 @@
+import threading;
+
+def display():
+    score = 589000
+    hi_score = 000000
+    print(f'SCORE {score} HI-SCORE {hi_score}\n\n')
+    print('B B B B B B B B B B B B B B\n')
+    print('B B B B B B B B B B B B B B\n')
+    print('B B B B B B B B B B B B B B\n')
+    print('B B B B B B B B B B B B B B\n')
+    print('B B B B B B B B B B B B B B\n\n\n')
+    print('  CCCC  CCCC  CCCC  CCCC\n')
+    print('---\n')
+
+display()
---
+++ player.py
@@ -0,0 +1,5 @@
+import threading;
+
+def player():
+    while True:
+        print('thread que controla o jogador e os disparos via teclado')

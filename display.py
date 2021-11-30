f220d3248b794828c651ad303feb103c53ed1d08
Hélio Neto <helioneto29@hotmail.com>
2021-11-30 15:07:51 -0300

    Aumentando a frequência da thread display para 10Hz

--- display.py
+++ display.py
@@ -8,5 +8,5 @@ def display():
         hiScore = 000000
         screen = f'SCORE {score} HI-SCORE {hiScore}\n\n{posicaoEnemies[0]:^4} {posicaoEnemies[1]:^4} {posicaoEnemies[2]:^4} {posicaoEnemies[3]:^4} {posicaoEnemies[4]:^4}\n{posicaoEnemies[5]:^4} {posicaoEnemies[6]:^4} {posicaoEnemies[7]:^4} {posicaoEnemies[8]:^4} {posicaoEnemies[9]:^4}\n{posicaoEnemies[10]:^4} {posicaoEnemies[11]:^4} {posicaoEnemies[12]:^4} {posicaoEnemies[13]:^4} {posicaoEnemies[14]:^4} {posicaoEnemies[15]:^4}\n\n  CCCC  CCCC  CCCC  CCCC\n{posicaoPlayer[0]}'
         print(screen)
-        time.sleep(0.5)
+        time.sleep(0.1)
         os.system('cls')

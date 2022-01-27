e22ed0bf8de0ad10ef63203fbea5f1829bd49174
Hélio Neto <helioneto29@hotmail.com>
2022-01-27 16:54:33 -0300

    Adicionando processo cloud_process

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import threading
+import threading, multiprocessing
 from display import display
 from enemies import enemies
 from player import player
 from interface_thread import interface_thread
 from logger_thread import logger_thread
+from cloud_process import cloud_process
 
 td = threading.Thread(target=display)
 tp = threading.Thread(target=player)
@@ -17,6 +18,17 @@ te.start()
 ti.start()
 tl.start()
 
+if __name__ == "__main__":
+    time_init = multiprocessing.Value('i')
+    score = multiprocessing.Value('i')
+    time_destroyed = multiprocessing.Value('i')
+    time_gameover = multiprocessing.Value('i')
+
+    p1 = multiprocessing.Process(target=cloud_process, args=(time_init, score, time_destroyed, time_gameover))
+    p1.start()
+    p1.join
+
 td.join()
+
 
 print('Fim de jogo')
